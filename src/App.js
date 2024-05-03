2ea531b08c7cacff6854c506c4b90aaa00381540
Minji Yun <gee2450@kookmin.ac.kr>
2024-05-03 20:56:47 +0900

    Feat: List 부분 컴포넌트 파일로 분리

--- src/App.js
+++ src/App.js
@@ -1,54 +1,10 @@
 import React, {Component, useState} from "react";
 import './App.css';
+import Lists from "./Lists";
 
 export default function App() {
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right"
-  }
-
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none"
-    };
-  }
-
-  const [todoData, setTodoData] = useState([
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true
-    },
-    {
-      id: "2",
-      title: "청소하기",
-      completed: false
-    },
-  ]);
+  const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
-
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id);
-    setTodoData(newTodoData)
-    console.log('newTodoData', newTodoData);
-  }
-
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    })
-    setTodoData(newTodoData)
-    console.log('newTodoData', newTodoData);
-  }
 
   const handleChange = (e) => {
     setValue(e.target.value)
@@ -73,15 +29,7 @@ export default function App() {
         <div className="title">
           <h1>할일 목록</h1>
         </div>
-        {
-          todoData.map((data) => 
-            <div style={getStyle(data.completed)} key={data.id}>
-              <input type="checkbox" defaultChecked={data.completed} onChange={() => handleCompleteChange(data.id)}/>
-                {data.title}
-              <button style={btnStyle} onClick={() => handleClick(data.id)}>x</button>
-            </div>
-          )
-        }
+        {Lists({todoData, setTodoData})}
         <form style={{display: "flex"}} onSubmit={handleSubmit}>
           <input
             type="text"
